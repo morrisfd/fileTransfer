f7869794a7217b95162eecceae4a0819517053b5
morrisfd <nsfrankem@gmail.com>
2020-04-27 14:12:00 -0500

    Updating test.sh and .gitignore

--- test.sh
+++ test.sh
@@ -11,5 +11,15 @@ PYTHONPATH=''
 export PYTHONPATH=$PYTHONPATH:$mainDir/src
 echo "PYTHONPATH: ${PYTHONPATH}"
 
+#set directories
+$testDir/tests/set_curr_dirs.ksh
+
+#Setup test data
+$testDir/tests/setup_data_dirs.ksh
+$testDir/tests/setup_fake_data.ksh
+
 coverage run -m unittest discover -s tests
 coverage report -m
+
+#reset directories
+$testDir/tests/reset_curr_dirs.ksh
